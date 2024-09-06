faf61c1be872d0582deca5c79fbaf01b3544cd45
Nawal123 <n.arkoubi@outlook.com>
2024-09-06 09:53:37 +0200

    Alles veranderd

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,20 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const buttons = document.querySelectorAll(".card-buttons button");
+const sections = document.querySelectorAll(".card-section");
+const card = document.querySelector(".card");
 
-function expand () {
-  document.body.classList.toggle('expand')
-}
+const handleButtonClick = e => {
+    const targetSection = e.target.getAttribute("data-section");
+    const section = document.querySelector(targetSection);
+    targetSection !== "#about" ?
+    card.classList.add("is-active") :
+    card.classList.remove("is-active");
+    card.setAttribute("data-state", targetSection);
+    sections.forEach(s => s.classList.remove("is-active"));
+    buttons.forEach(b => b.classList.remove("is-active"));
+    e.target.classList.add("is-active");
+    section.classList.add("is-active");
+};
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", handleButtonClick);
+});
